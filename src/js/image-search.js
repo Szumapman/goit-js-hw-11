348a7ad1364ec90059e8eaadfdd60c0f2bfeb8f9
Paweł Szumański <pawel.szumanski@outlook.com>
2024-12-26 18:59:05 +0100

    add lightbox

--- src/js/image-search.js
+++ src/js/image-search.js
@@ -2,12 +2,20 @@
 
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-import { getPictures } from "./get-pixabay-pictures";
-import { createGallery } from "./create-gallery";
+// to use a different image source, just create new functions getPictures and createGallery and change these two imports
+import { getPictures } from "./get-pixabay-pictures"; 
+import { createGallery } from "./create-pixabay-gallery";
 
 const form = document.querySelector(".image-search-form");
 const gallery = document.querySelector(".gallery");
+const modalLightbox = new SimpleLightbox(".gallery-item-link", {
+    captions: true,
+    captionsData: "alt",
+    captionDelay: 250,  
+});
 
 form.addEventListener("submit", evt => {
     evt.preventDefault();
@@ -15,13 +23,13 @@ form.addEventListener("submit", evt => {
     const searchTerm = form.elements.searchTerm.value.trim();
 
     getPictures(searchTerm).then(response => {
-        console.log(response);
         if (response.totalHits === 0) {
             noMatchingResults();
             form.reset();
             return;
         }
         gallery.appendChild(createGallery(response.hits));
+        modalLightbox.refresh();
         form.reset();
     });
 
